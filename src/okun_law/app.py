1573cff7218fd90e8925d53c166ec619349d97d1
Christophe Benz <christophe.benz@nomics.world>
2024-06-25 15:28:24 +0200

    Reformat files

--- src/okun_law/app.py
+++ src/okun_law/app.py
@@ -1,66 +1,71 @@
-# app.py
-import streamlit as st
-from data_loader import load_data
-from chart_creator import create_okun_curve
 import pandas as pd
+import streamlit as st
+from chart_creator import create_okun_curve
+from data_loader import load_data
 
 
 def filter_by_date(df, start_date, end_date):
-    df['period'] = pd.to_datetime(df['period'])
-    return df[(df['period'] >= start_date) & (df['period'] <= end_date)]
+    df["period"] = pd.to_datetime(df["period"])
+    return df[(df["period"] >= start_date) & (df["period"] <= end_date)]
 
 
 def main():
-    st.set_page_config(
-        page_title="DBnomics Okun's Law",
-        page_icon="DB_logo")
-    st.image("dbnomics_2.svg", width=300)
+    st.set_page_config(page_title="DBnomics Okun's Law", page_icon="favicon.png")
+    st.image("dbnomics.svg", width=300)
     st.title(":blue[Okun's Law]")
 
     tab1, tab2 = st.tabs(["Explanations", "Charts"])
 
     with tab1:
         st.header(":blue[Explanations]")
-        st.write("\n"
-                 "Okun's Law was conceived in 1962 by the Keynesian American economist Arthur Okun (1928-1980).\n"
-                 "He was professor at Yale and advisor to Presidents Kennedy and Johnson.\n"
-                 "\n"
-                 "Okun's Law highlights a linear relationship between the economic growth rate and the change in the unemployment rate.\n"
-                 "Below a certain threshold of GDP growth, the unemployment rate increases.\n "
-                 "Beyond this same threshold, the unemployment rate decreases.\n"
-                 "This law thus allows for the prediction of the effects of accelerated economic growth on unemployment.\n"
-                 "\n"
-                 "Thanks to the GDP growth coefficient, it is possible to know the necessary growth for stabilizing or even reducing unemployment.\n"
-                 "Okun asserted that in the United States, for every 3% increase in economic growth beyond the long-term growth of an economy, the unemployment rate would decrease by 1% (and vice versa).\n"
-                 "This 1% variation in GDP is broken down as follows: \n"
-                 "- 1/3 in the form of productivity \n"
-                 "- 2/3 in the form of aggregated hours (employment + hours worked)\n"
-                 "\n"
-                 "There is therefore a complex relationship between the labor market and the goods and services market.\n"
-                 "However, this assertion became false starting from the 1970s with the beginning of the stagflation phenomenon (stagnation of economic activity, high unemployment, and rising inflation).\n"
-                 "\n"
-                 "Another major event, the Great Recession of 2007-2009, allowed for testing the validity of Okun's Law. During this period, there was an increase in productivity in the United States, which contradicts Okun's assertions: competition between companies was so strong that they were forced to maintain their productivity.\n"
-                 "Conversely, in France, Okun's Law is better adhered to: there was a 2% decline in French productivity. \n"
-                 "\n"
-                 "Thus, in France, the breakdown defined by Okun seems relatively respected, while it is no longer the case in the United States. This difference is due to the greater rigidity of the French labor market, which practices labor hoarding.\n"
-                 'Labor hoarding can be defined as "that part of labor input which is not fully utilized by a company during its production process at any given point in time".\n'
-                 "The management practiced in the United States renders Okun's Law obsolete. Nevertheless, during periods of increased uncertainty, Okun's Law loses its explanatory power concerning the labor market.\n"
-                 "\n")
+        st.write(
+            "\n"
+            "Okun's Law was conceived in 1962 by the Keynesian American economist Arthur Okun (1928-1980).\n"
+            "He was professor at Yale and advisor to Presidents Kennedy and Johnson.\n"
+            "\n"
+            "Okun's Law highlights a linear relationship between the economic growth rate and the change in the unemployment rate.\n"
+            "Below a certain threshold of GDP growth, the unemployment rate increases.\n "
+            "Beyond this same threshold, the unemployment rate decreases.\n"
+            "This law thus allows for the prediction of the effects of accelerated economic growth on unemployment.\n"
+            "\n"
+            "Thanks to the GDP growth coefficient, it is possible to know the necessary growth for stabilizing or even reducing unemployment.\n"
+            "Okun asserted that in the United States, for every 3% increase in economic growth beyond the long-term growth of an economy, the unemployment rate would decrease by 1% (and vice versa).\n"
+            "This 1% variation in GDP is broken down as follows: \n"
+            "- 1/3 in the form of productivity \n"
+            "- 2/3 in the form of aggregated hours (employment + hours worked)\n"
+            "\n"
+            "There is therefore a complex relationship between the labor market and the goods and services market.\n"
+            "However, this assertion became false starting from the 1970s with the beginning of the stagflation phenomenon (stagnation of economic activity, high unemployment, and rising inflation).\n"
+            "\n"
+            "Another major event, the Great Recession of 2007-2009, allowed for testing the validity of Okun's Law. During this period, there was an increase in productivity in the United States, which contradicts Okun's assertions: competition between companies was so strong that they were forced to maintain their productivity.\n"
+            "Conversely, in France, Okun's Law is better adhered to: there was a 2% decline in French productivity. \n"
+            "\n"
+            "Thus, in France, the breakdown defined by Okun seems relatively respected, while it is no longer the case in the United States. This difference is due to the greater rigidity of the French labor market, which practices labor hoarding.\n"
+            'Labor hoarding can be defined as "that part of labor input which is not fully utilized by a company during its production process at any given point in time".\n'
+            "The management practiced in the United States renders Okun's Law obsolete. Nevertheless, during periods of increased uncertainty, Okun's Law loses its explanatory power concerning the labor market.\n"
+            "\n"
+        )
 
     with tab2:
         st.header(":blue[Okun's Law Per Country]")
-        countries = ['France', 'United States', 'Argentina', 'Japan', 'Spain', 'India']
-        selected_countries = st.multiselect('**Select Country**', countries, default=['France'])
+        countries = ["France", "United States", "Argentina", "Japan", "Spain", "India"]
+        selected_countries = st.multiselect(
+            "**Select Country**", countries, default=["France"]
+        )
 
         data = load_data()
 
         if selected_countries:
-            all_dates = pd.to_datetime(data[selected_countries[0]]['period']).sort_values().unique()
+            all_dates = (
+                pd.to_datetime(data[selected_countries[0]]["period"])
+                .sort_values()
+                .unique()
+            )
             start_date, end_date = st.select_slider(
-                '**Select Date Range**',
+                "**Select Date Range**",
                 options=all_dates,
                 value=(all_dates[0], all_dates[-1]),
-                format_func=lambda x: x.strftime('%Y')
+                format_func=lambda x: x.strftime("%Y"),
             )
 
             for country in selected_countries:
@@ -69,9 +74,13 @@ def main():
                     df_filtered = filter_by_date(df, start_date, end_date)
                     fig = create_okun_curve(df_filtered, country)
                     st.plotly_chart(fig)
-        st.subheader(':blue[**DBnomics sources**]')
-        st.write(":blue[Unemploymant rate]: [*link*](https://db.nomics.world/ILO/UNE_DEAP_SEX_AGE_EDU_RT?tab=list)")
-        st.write(":blue[GDP growth rate]: [*link*](https://db.nomics.world/IMF/WEO:2024-04?tab=list)")
+        st.subheader(":blue[**DBnomics sources**]")
+        st.write(
+            ":blue[Unemploymant rate]: [*link*](https://db.nomics.world/ILO/UNE_DEAP_SEX_AGE_EDU_RT?tab=list)"
+        )
+        st.write(
+            ":blue[GDP growth rate]: [*link*](https://db.nomics.world/IMF/WEO:2024-04?tab=list)"
+        )
 
 
 if __name__ == "__main__":
--- src/okun_law/data_loader.py
+++ src/okun_law/data_loader.py
@@ -2,42 +2,52 @@
 import pandas as pd
 from dbnomics import fetch_series
 
+
 def load_data():
-    df_unemploy = fetch_series([
-        'ILO/UNE_DEAP_SEX_AGE_EDU_RT/USA.BA_453.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A',
-        'ILO/UNE_DEAP_SEX_AGE_EDU_RT/FRA.BA_148.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A',
-        'ILO/UNE_DEAP_SEX_AGE_EDU_RT/ARG.BA_150.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A',
-        'ILO/UNE_DEAP_SEX_AGE_EDU_RT/JPN.BA_259.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A',
-        'ILO/UNE_DEAP_SEX_AGE_EDU_RT/ESP.BA_2244.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A',
-        'ILO/UNE_DEAP_SEX_AGE_EDU_RT/IND.BA_14121.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A',
-    ])
+    df_unemploy = fetch_series(
+        [
+            "ILO/UNE_DEAP_SEX_AGE_EDU_RT/USA.BA_453.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A",
+            "ILO/UNE_DEAP_SEX_AGE_EDU_RT/FRA.BA_148.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A",
+            "ILO/UNE_DEAP_SEX_AGE_EDU_RT/ARG.BA_150.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A",
+            "ILO/UNE_DEAP_SEX_AGE_EDU_RT/JPN.BA_259.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A",
+            "ILO/UNE_DEAP_SEX_AGE_EDU_RT/ESP.BA_2244.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A",
+            "ILO/UNE_DEAP_SEX_AGE_EDU_RT/IND.BA_14121.AGE_YTHADULT_YGE15.EDU_AGGREGATE_TOTAL.SEX_T.A",
+        ]
+    )
 
-    df_gdp = fetch_series([
-        'IMF/WEO:2024-04/USA.NGDP_RPCH.pcent_change',
-        'IMF/WEO:2024-04/FRA.NGDP_RPCH.pcent_change',
-        'IMF/WEO:2024-04/ARG.NGDP_RPCH.pcent_change',
-        'IMF/WEO:2024-04/JPN.NGDP_RPCH.pcent_change',
-        'IMF/WEO:2024-04/ESP.NGDP_RPCH.pcent_change',
-        'IMF/WEO:2024-04/IND.NGDP_RPCH.pcent_change'
-    ])
+    df_gdp = fetch_series(
+        [
+            "IMF/WEO:2024-04/USA.NGDP_RPCH.pcent_change",
+            "IMF/WEO:2024-04/FRA.NGDP_RPCH.pcent_change",
+            "IMF/WEO:2024-04/ARG.NGDP_RPCH.pcent_change",
+            "IMF/WEO:2024-04/JPN.NGDP_RPCH.pcent_change",
+            "IMF/WEO:2024-04/ESP.NGDP_RPCH.pcent_change",
+            "IMF/WEO:2024-04/IND.NGDP_RPCH.pcent_change",
+        ]
+    )
 
-    col_em = ['original_period', 'original_value', 'Reference area']
-    col_gdp = ['original_period', 'original_value', 'WEO Country']
+    col_em = ["original_period", "original_value", "Reference area"]
+    col_gdp = ["original_period", "original_value", "WEO Country"]
 
-    df_unemploy = df_unemploy.rename(columns={'original_value': 'unemployment_rate'})
-    df_gdp = df_gdp.rename(columns={'original_value': 'gdp_growth_rate'})
+    df_unemploy = df_unemploy.rename(columns={"original_value": "unemployment_rate"})
+    df_gdp = df_gdp.rename(columns={"original_value": "gdp_growth_rate"})
 
     def create_country_df(df, country, col, col_name):
-        return df[df[col] == country][['original_period', col_name]].rename(
-            columns={'original_period': 'period', col_name: col_name})
+        return df[df[col] == country][["original_period", col_name]].rename(
+            columns={"original_period": "period", col_name: col_name}
+        )
 
-    countries = ['France', 'United States', 'Argentina', 'Japan', 'Spain', 'India']
+    countries = ["France", "United States", "Argentina", "Japan", "Spain", "India"]
     df_dict = {}
 
     for country in countries:
-        df_em = create_country_df(df_unemploy, country, 'Reference area', 'unemployment_rate')
-        df_gdp_ctry = create_country_df(df_gdp, country, 'WEO Country', 'gdp_growth_rate')
-        df_merged = pd.merge(df_em, df_gdp_ctry, on='period')
+        df_em = create_country_df(
+            df_unemploy, country, "Reference area", "unemployment_rate"
+        )
+        df_gdp_ctry = create_country_df(
+            df_gdp, country, "WEO Country", "gdp_growth_rate"
+        )
+        df_merged = pd.merge(df_em, df_gdp_ctry, on="period")
         df_dict[country] = df_merged
 
     return df_dict
